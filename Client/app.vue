023f210a3d71d3e045bae50b5cd741f5277de058
Wlad Kowalew <wladickowalew@gmail.com>
2021-03-14 13:13:55 +0300

    7.2.3 commit

--- Client/app.vue
+++ Client/app.vue
@@ -1,5 +1,13 @@
 <template>
 	<div>
 		Hello World!
+		<c-tag></c-tag>
 	</div>
 </template>
+
+<script>
+	let Header = require("./header.vue");
+	module.exports = {
+		components:{cTag: Header}
+	};
+</script>
---
+++ Client/header.vue
@@ -0,0 +1,21 @@
+<style scoped>
+	h1{
+		color: blue;
+	}
+</style>
+
+<template>
+	<div>
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		data:function () {
+			return{
+				msg: "Hello, component!"
+			};
+		}
+	};
+</script>
