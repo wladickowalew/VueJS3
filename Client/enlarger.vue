b4072b52367ec3d56016b470ee3e3148fee1725a
Wlad Kowalew <wladickowalew@gmail.com>
2021-03-21 12:25:10 +0300

    dfhsdh

---
+++ Client/enlarger.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1 v-on:mousemove="enlarge"> {{number}}</h1>
+</template>
+
+<script>
+	module.exports = {
+		data:function () {
+			return {number: 0};
+		},
+		methods:{
+			enlarge: function(){
+				this.number += 1;
+			}
+		}
+	};
+</script>
