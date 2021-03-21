5e47dce9ec76f2cf9b804300c6f460ab6d4df3d7
Wlad Kowalew <wladickowalew@gmail.com>
2021-03-21 12:28:06 +0300

    Create reducer.vue

---
+++ Client/reducer.vue
@@ -0,0 +1,16 @@
+<template>
+	<button type="button" v-on:click="reduce">{{number}}</button>
+</template>
+
+<script>
+	module.exports = {
+		data:function () {
+			return {number: 0};
+		},
+		methods:{
+			reduce: function(){
+				this.number -= 1;
+			}
+		}
+	};
+</script>
